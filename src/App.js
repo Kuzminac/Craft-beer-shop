e6b285989d90904eca9bf5d77c2d91b742934abf
Nemanja <nemanjakuzminac21@gmail.com>
2022-04-09 14:37:15 +0200

    Pages setup and added Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css'
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Search from './pages/search/Search'
+import Create from './pages/create/Create'
+import Beer from './pages/beer/Beer'
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/beers/:id">
+            <Beer />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/beer/Beer.js
@@ -0,0 +1,9 @@
+import './Beer.css'
+
+import React from 'react'
+
+export default function Beer() {
+  return (
+    <div>Beer</div>
+  )
+}
