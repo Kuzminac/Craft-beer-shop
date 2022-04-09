c370176336bab4ca5bfd9acf68f84ba2a75b639a
Nemanja <nemanjakuzminac21@gmail.com>
2022-04-09 16:08:39 +0200

    fetched data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,18 @@
 import './Home.css'
 
+import { useFetch } from '../../hooks/useFetch'
+
 import React from 'react'
 
 export default function Home() {
+  const { data, isPending, error } = useFetch("http://localhost:3000/beers")
   return (
-    <div>Home</div>
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && data.map(beer => (
+        <h2 key={beer.id}>{beer.title}</h2>
+      ))}
+    </div>
   )
 }
