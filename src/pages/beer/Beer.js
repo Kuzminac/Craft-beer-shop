4ed29ab1d796b39ba7447ad30714cec83fc44b1f
Nemanja <nemanjakuzminac21@gmail.com>
2022-04-10 08:58:53 +0200

    added Beer component

--- src/pages/beer/Beer.js
+++ src/pages/beer/Beer.js
@@ -1,9 +1,20 @@
 import './Beer.css'
 
+import { useFetch } from '../../hooks/useFetch'
+import { useParams } from 'react-router-dom'
+
 import React from 'react'
 
 export default function Beer() {
+  const { id } = useParams()
+  const url = "http://localhost:3000/beers/" + id
+  const { error, isPending, data: beer } = useFetch(url)
+
   return (
-    <div>Beer</div>
+    <div className='beer'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {beer && <h1>{beer.title}</h1>}
+    </div>
   )
 }
