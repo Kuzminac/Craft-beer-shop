1d5fd5cc6e6e5ac1903b2f9cbbd0e68949c18263
Nemanja <nemanjakuzminac21@gmail.com>
2022-04-12 08:39:00 +0200

    connected to Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDjSCMRN_thrvO7a7tAixD2b2RaO9tjfzE",
+    authDomain: "beerhouse-3000.firebaseapp.com",
+    projectId: "beerhouse-3000",
+    storageBucket: "beerhouse-3000.appspot.com",
+    messagingSenderId: "630665426143",
+    appId: "1:630665426143:web:63600fd0e02e24f229e56a"
+  };
+
+  // init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  // init services
+  const projectFirestore = firebase.firestore()
+
+  export { projectFirestore }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,36 @@
 import './Home.css'
 
-import { useFetch } from '../../hooks/useFetch'
+import { projectFirestore } from '../../firebase/config'
 
 import React from 'react'
 import BeerList from '../../components/BeerList'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const { data, isPending, error } = useFetch("http://localhost:3000/beers")
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true)
+
+    projectFirestore.collection('beers').get().then((snapshot) => {
+      if (snapshot.empty) {
+        setError('No beers to load')
+        setIsPending(false)
+      } else {
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({ id: doc.id, ...doc.data() })
+        })
+        setData(results)
+        setIsPending(false)
+      }
+    }).catch(err => {
+      setError(err.message)
+    })
+  },[])
+
   return (
     <div className='home'>
       {error && <p className='error'>{error}</p>}
