1c28d548239156775e478866290dacafd050188e
Nemanja <nemanjakuzminac21@gmail.com>
2022-04-29 18:46:39 +0200

    final touches

--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -1,11 +1,14 @@
 import './BeerList.css'
 import { Link } from 'react-router-dom'
 import Trashcan from '../assets/img/trashcan.svg'
+import { useAuthContext } from '../hooks/useAuthContext'
 import { projectFirestore } from '../firebase/config'
 
 import React from 'react'
 
 export default function BeerList({ beers }) {
+  const { user } = useAuthContext()
+  
   const handleClick = (id) => {
     projectFirestore.collection('beers').doc(id).delete()
   }
@@ -17,14 +20,16 @@ export default function BeerList({ beers }) {
                 <p>type: <span>{beer.type}</span> </p>
                 <p>distributor: <span>{beer.distributor}</span></p>
                 <p>abv: <span>{beer.abv}</span> </p>
-                <p><span>{beer.description}</span></p>
+                <p><span>{beer.description.substring(0, 100)}...</span></p>
                 <Link to={`/beers/${beer.id}`}>See more</Link>
-                <img 
-                  className='delete'
-                  alt='delete icon'
-                  src={Trashcan}
-                  onClick={() => handleClick(beer.id)}
-                />
+                {user && (
+                  <img 
+                    className='delete'
+                    alt='delete icon'
+                    src={Trashcan}
+                    onClick={() => handleClick(beer.id)}
+                  />
+                )}
             </div>
         ))}
     </div>
